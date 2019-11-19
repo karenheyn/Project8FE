3b63421a3fa02b2581ab166c6f8a6657046821ab
karenheyn <karenheyn@gmail.com>
2019-11-19 14:40:44 -0500

    done with storybook

--- src/stories/Forms/Forms.js
+++ src/stories/Forms/Forms.js
@@ -21,17 +21,20 @@ const Form = props => {
   return (
     <form className={classList}>
       <input className={props.name} placeholder='name'></input>
-      <select className='rating'>
-        <option value='1'>1</option>
-        <option value='1.5'>1.5</option>
-        <option value='2'>2</option>
-        <option value='2.5'>2.5</option>
-        <option value='3'>3</option>
-        <option value='3.5'>3.5</option>
-        <option value='4'>4</option>
-        <option value='4.5'>4.5</option>
-        <option value='5'>5</option>
-      </select>
+      <div className='rating-div'>
+        <label for='rating'>rating:</label>
+        <select className='rating'>
+          <option value='1'>1</option>
+          <option value='1.5'>1.5</option>
+          <option value='2'>2</option>
+          <option value='2.5'>2.5</option>
+          <option value='3'>3</option>
+          <option value='3.5'>3.5</option>
+          <option value='4'>4</option>
+          <option value='4.5'>4.5</option>
+          <option value='5'>5</option>
+        </select>
+      </div>
       <textarea className='review' placeholder='write your review'></textarea>
 
       <Button label='Submit' type='sumbit'></Button>
