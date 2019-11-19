7a9491935b3f362c888a191de2ed5da004089ef0
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-11-19 16:13:43 -0500

    added mapping of props.data to render so we can use the data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,60 +10,60 @@ import "./App.css";
 let url = "https://dc-100-restaurants-db.herokuapp.com/restaurants";
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			data: [],
-			rName: [],
-			sideDrawerOpen: false,
-			isLoading: true
-		};
-	}
-	componentDidMount() {
-		fetch(url)
-			.then(res => res.json())
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      rName: [],
+      sideDrawerOpen: false,
+      isLoading: true
+    };
+  }
+  componentDidMount() {
+    fetch(url)
+      .then(res => res.json())
 
-			.then(res => {
-				console.log(res);
-				this.setState({ data: res, isLoading: false });
-			})
-			.catch(err => {
-				console.error(err);
-			});
-	}
-	drawerToggleClickHandler = () => {
-		this.setState(prevState => {
-			return { sideDrawerOpen: !prevState.sideDrawerOpen };
-		});
-	};
-	backdropClickHandler = () => {
-		this.setState({ sideDrawerOpen: false });
-	};
-	render() {
-		console.log(this.state.data);
-		for (let i = 0; i < this.state.data.length; i++) {
-			this.state.rName.push(this.state.data[i].name);
-		}
-		let backdrop;
-		if (this.state.sideDrawerOpen) {
-			backdrop = <Backdrop click={this.backdropClickHandler} />;
-		}
-		if (!this.state.loading) {
-			return (
-				<div className='main-container'>
-					<Navbar drawerClickHandler={this.drawerToggleClickHandler} />
-					<SideDrawer show={this.state.sideDrawerOpen} />
-					{backdrop}
-					<main>
-						<Route path='/' render={() => <Home data={this.state.data} />} />
-						<Route path='/searchresults' component={SearchResults} />
-						<Route path='/:restaurant' component={Restaurant} />
-					</main>
-				</div>
-			);
-		}
-		return <div>Loading</div>;
-	}
+      .then(res => {
+        // console.log(res);
+        this.setState({ data: res, isLoading: false });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+  drawerToggleClickHandler = () => {
+    this.setState(prevState => {
+      return { sideDrawerOpen: !prevState.sideDrawerOpen };
+    });
+  };
+  backdropClickHandler = () => {
+    this.setState({ sideDrawerOpen: false });
+  };
+  render() {
+    // console.log(this.state.data);
+    for (let i = 0; i < this.state.data.length; i++) {
+      this.state.rName.push(this.state.data[i].name);
+    }
+    let backdrop;
+    if (this.state.sideDrawerOpen) {
+      backdrop = <Backdrop click={this.backdropClickHandler} />;
+    }
+    if (!this.state.loading) {
+      return (
+        <div className="main-container">
+          <Navbar drawerClickHandler={this.drawerToggleClickHandler} />
+          <SideDrawer show={this.state.sideDrawerOpen} />
+          {backdrop}
+          <main>
+            <Route path="/" render={() => <Home data={this.state.data} />} />
+            <Route path="/searchresults" component={SearchResults} />
+            <Route path="/:restaurant" component={Restaurant} />
+          </main>
+        </div>
+      );
+    }
+    return <div>Loading</div>;
+  }
 }
 
 export default App;
