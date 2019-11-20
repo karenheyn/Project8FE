cd07f76d9f500e0ed8ba125df26ad5a15f8036ad
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-11-20 14:22:51 -0500

    changed bottom images to be dynamically rendered, gave them styling

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,32 +3,42 @@ import "./Home.css";
 import Carousel from "./Carousel/Carousel";
 import TopFive from "../TopFiveListings/TopFiveListings";
 import { array, arrayOf } from "prop-types";
+import RestaurantImageBox from "../../components/RestaurantImageBox/RestaurantImageBox";
+import Restaurant from "../Restaurant/Restaurant";
 
 class Home extends Component {
-	constructor(props) {
-		super(props);
-		// console.log(this.props.data);
-		this.state = {
-			searchBar: "",
-			data: this.props.data
-		};
-	}
-	handleInputChange = e => {
-		e.preventDefault();
-		this.setState({ searchBar: e.target.value });
-	};
-	handleSubmit = e => {
-		e.preventDefault();
-	};
+  constructor(props) {
+    super(props);
+    // console.log(this.props.data);
+    this.state = {
+      searchBar: "",
+      data: this.props.data
+    };
+  }
+  handleInputChange = e => {
+    e.preventDefault();
+    this.setState({ searchBar: e.target.value });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+  };
 
-<<<<<<< HEAD
   render() {
     let arrayOfData = [];
     this.props.data.map(item => {
       arrayOfData.push(item);
     });
-    console.log(this.state.searchBar);
-    console.log(arrayOfData);
+    let ratings = [];
+    arrayOfData.map(item => {
+      if (item.rating > 4.6) {
+        ratings.push(item);
+      }
+    });
+    // {
+    // 	for (let i = 0; i < arrayOfData.length; i++)
+    // 		if (arra);
+    // }
+    // console.log(arrayOfData);
     if (!arrayOfData.length < 1) {
       return (
         <div className="homepage">
@@ -44,65 +54,23 @@ class Home extends Component {
           <div className="slider">
             <Carousel className="carousel" data={arrayOfData}></Carousel>
           </div>
-          <div className="most-reviewed-title">
-            <h2>{arrayOfData[9].name}</h2>
-          </div>
-          <div className="most-reviewed">
-            <img src="https://via.placeholder.com/200" alt="placeholder" />
-            <img src="https://via.placeholder.com/200" alt="placeholder" />
-            <img src="https://via.placeholder.com/200" alt="placeholder" />
-            <img src="https://via.placeholder.com/200" alt="placeholder" />
-            <img src="https://via.placeholder.com/200" alt="placeholder" />
+
+          <div className="top-rated-rests">
+            {arrayOfData.slice(0, 5).map(item => {
+              return (
+                <RestaurantImageBox
+                  imageUrl={item.imageUrl}
+                  name={item.name}
+                  yelpUrl={item.yelpUrl}
+                />
+              );
+            })}
           </div>
         </div>
       );
     }
     return <div>Loading</div>;
   }
-=======
-	render() {
-		let arrayOfData = [];
-		this.props.data.map(item => {
-			arrayOfData.push(item);
-		});
-		let ratings = [];
-		arrayOfData.map(item => {
-			if (item.rating > 4.6) {
-				ratings.push(item);
-			}
-		});
-		// {
-		// 	for (let i = 0; i < arrayOfData.length; i++)
-		// 		if (arra);
-		// }
-		// console.log(arrayOfData);
-		if (!arrayOfData.length < 1) {
-			return (
-				<div className='homepage'>
-					<h1>Home Page</h1>
-					<form className='homepage-searchbar' onSubmit={this.handleSubmit}>
-						<input
-							type='text'
-							name='searchBar'
-							onChange={this.handleChange}
-						></input>
-						<button type='submit'>Seach</button>
-					</form>
-					<div className='slider'>
-						<Carousel className='carousel' data={arrayOfData}></Carousel>
-					</div>
-					<div className='most-reviewed-title'>
-						<h2>{arrayOfData[9].name}</h2>
-					</div>
-					<div className='most-reviewed'>
-						<TopFive data={ratings} />
-					</div>
-				</div>
-			);
-		}
-		return <div>Loading</div>;
-	}
->>>>>>> b2351e7c70c3553cae568dbe2bd9757c00514f95
 }
 export default Home;
 // componentDidMount() {
