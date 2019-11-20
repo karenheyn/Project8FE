e83dc196d0174d1bfb3a63d06e700d207e2fc8d0
karenheyn <karenheyn@gmail.com>
2019-11-20 10:48:04 -0500

    tried something

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,27 +1,53 @@
 import React, { Component } from "react";
 import "./Home.css";
 import Carousel from "./Carousel/Carousel";
-
+import axios from "axios";
 // I LIKE BIG BUTTS AND I CANNOT LIE!!!!!
 
+import SearchResults from "../SearchResults/SearchResults";
+let namesArray = [];
 class Home extends Component {
   constructor(props) {
     super(props);
+
     // console.log(this.props.data);
     this.state = {
       searchBar: "",
-      data: this.props.data
+      data: this.props.data,
+      results: []
     };
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
+  search = e => {
+    e.preventDefault();
+    axios
+      .get(
+        `https://dc-100-restaurants-db.herokuapp.com/restaurants/name/${this.state.searchBar}`
+      )
+      .then(res => res.data)
+      .then(res => {
+        if (!res.Search) {
+          this.setState({ results: [] });
+          return;
+        }
+
+        const results = res.Search.map(result => result.name);
+        this.setState({
+          results
+        });
+      });
+  };
   handleInputChange = e => {
     e.preventDefault();
     this.setState({ searchBar: e.target.value });
   };
+
   handleSubmit = e => {
     e.preventDefault();
   };
 
   render() {
+    const { results } = this.state;
     let arrayOfData = [];
     this.props.data.map(item => {
       arrayOfData.push(item);
@@ -32,14 +58,24 @@ class Home extends Component {
       return (
         <div className='homepage'>
           <h1>Home Page</h1>
-          <form className='homepage-searchbar' onSubmit={this.handleSubmit}>
+          <form className='homepage-searchbar' onSubmit={this.search}>
             <input
               type='text'
               name='searchBar'
-              onChange={this.handleChange}
+              onChange={this.handleInputChange}
             ></input>
             <button type='submit'>Seach</button>
           </form>
+          {results.length > 0 ? (
+            results.map(result => (
+              <SearchResults search={result} key={result} />
+            ))
+          ) : (
+            <p>
+              Couldn't find any result. Please search again using another search
+              criteria.
+            </p>
+          )}
           <div className='slider'>
             <Carousel className='carousel' data={arrayOfData}></Carousel>
           </div>
--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,17 +1,63 @@
 import React, { Component } from "react";
 import "./SearchResults.css";
+import axios from "axios";
 
 class SearchResults extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
-	render() {
-		return (
-			<div>
-				<h1>Results</h1>
-			</div>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      resultsData: {}
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        `https://dc-100-restaurants-db.herokuapp.com/restaurants/name/${this.props.search}`
+      )
+      .then(res => res.data)
+      .then(res => {
+        this.setState({ resultsData: res });
+      });
+  }
+
+  render() {
+    const {
+      imageUrl,
+      name,
+      catagories,
+      address,
+      city,
+      phone,
+      rating
+    } = this.state.resultsData;
+
+    // if (!Poster || Poster === "N/A") {
+    //   return null;
+    // }
+
+    return (
+      <div className='card-container'>
+        <div className='image-container'>
+          <div
+            className='bg-image'
+            style={{ backgroundImage: `url(${imageUrl})` }}
+          />
+        </div>
+        <div className='result-info'>
+          <h2>Movie Details</h2>
+          <div>
+            <h1>{name}</h1>
+            <small>Cuisine: {catagories}</small>
+          </div>
+          <h4>Rating: {rating} / 5</h4>
+          <p>
+            {address} {city}
+          </p>
+          <div className='tags-container'>{phone}</div>
+        </div>
+      </div>
+    );
+  }
 }
 export default SearchResults;
