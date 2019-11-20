a4165bda5f4660da200a48d13e4daaccbe9fe230
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-11-20 11:13:07 -0500

    updated nothing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -29,33 +29,28 @@ class Home extends Component {
     console.log(arrayOfData);
     if (!arrayOfData.length < 1) {
       return (
-        <div className='homepage'>
+        <div className="homepage">
           <h1>Home Page</h1>
-          <form className='homepage-searchbar' onSubmit={this.handleSubmit}>
+          <form className="homepage-searchbar" onSubmit={this.handleSubmit}>
             <input
-              type='text'
-              name='searchBar'
+              type="text"
+              name="searchBar"
               onChange={this.handleChange}
             ></input>
-            <button type='submit'>Seach</button>
+            <button type="submit">Seach</button>
           </form>
-          <div className='slider'>
-            <img src='https://via.placeholder.com/800x350' alt='placeholder' />
+          <div className="slider">
+            <img src="https://via.placeholder.com/800x350" alt="placeholder" />
           </div>
-<<<<<<< HEAD
-          <div className='most-reviewed-title'>
-            <h2>{arrayOfData[5].name}</h2>
-=======
           <div className="most-reviewed-title">
             <h2>{arrayOfData[9].name}</h2>
->>>>>>> 6c2ebd655d48d299143963fb05824ce39b144f97
           </div>
-          <div className='most-reviewed'>
-            <img src='https://via.placeholder.com/200' alt='placeholder' />
-            <img src='https://via.placeholder.com/200' alt='placeholder' />
-            <img src='https://via.placeholder.com/200' alt='placeholder' />
-            <img src='https://via.placeholder.com/200' alt='placeholder' />
-            <img src='https://via.placeholder.com/200' alt='placeholder' />
+          <div className="most-reviewed">
+            <img src="https://via.placeholder.com/200" alt="placeholder" />
+            <img src="https://via.placeholder.com/200" alt="placeholder" />
+            <img src="https://via.placeholder.com/200" alt="placeholder" />
+            <img src="https://via.placeholder.com/200" alt="placeholder" />
+            <img src="https://via.placeholder.com/200" alt="placeholder" />
           </div>
         </div>
       );
