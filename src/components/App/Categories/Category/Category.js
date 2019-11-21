d1b5a2361d32e0a620f966536179f04360b50545
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-11-21 10:19:18 -0500

    added click function to cuisine categories

--- src/components/App/Categories/Category/Category.js
+++ src/components/App/Categories/Category/Category.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import RestaurantImageBox from "../../../RestaurantImageBox/RestaurantImageBox";
 import "./Category.css";
+import RestaurantDetail from "../../../RestaurantDetail/RestaurantDetail";
 const baseurl =
   "https://dc-100-restaurants-db.herokuapp.com/restaurants/category/";
 
@@ -8,7 +9,9 @@ class Brands extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: []
+      data: [],
+      renderDetails: false,
+      currentData: {} //for getClickedRestaurantData - tyler
     };
   }
   componentDidMount() {
@@ -20,19 +23,47 @@ class Brands extends Component {
         this.setState({ data: res });
       });
   }
+  /**
+   * getClickedRestaurantData returns the data from a restaurant that is clicked
+   * -Tyler
+   */
+  getClickedRestaurantData = data => {
+    // console.log(data);
+    this.setState({ currentData: data, renderDetails: true });
+  };
+  closeClickedRestaurantDetails = doClose => {
+    if (doClose) {
+      this.setState({ renderDetails: false });
+    }
+  };
   render() {
     const data = [];
     console.log(this.state.data);
     this.state.data.map(item => data.push(item));
     return (
-      <div className='wrapper-div'>
+      <div className="wrapper-div">
         {data.map(item => {
           return (
-            <div className='padding-control'>
-              <RestaurantImageBox data={item} />
+            <div className="padding-control">
+              <RestaurantImageBox
+                key={item.name}
+                data={item}
+                getClickedRestaurantData={this.getClickedRestaurantData.bind(
+                  this
+                )}
+              />
             </div>
           );
         })}
+
+        {this.state.renderDetails ? (
+          <RestaurantDetail
+            currentData={this.state.currentData}
+            closeClickedRestaurantDetails={this.closeClickedRestaurantDetails.bind(
+              this
+            )}
+          />
+        ) : null}
       </div>
     );
   }
