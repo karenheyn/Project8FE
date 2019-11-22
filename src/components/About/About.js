172abb81af00cd9e73778c26c26580a87f1f38b3
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-11-22 14:29:38 -0500

    working on about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,20 +1,51 @@
 import React from "react";
-import Washington from "../../assets/washington.jpg";
+import Washington from "../../assets/washingtonimage.jpg";
 import "./About.css";
 const About = () => {
   return (
-    <div className='about-container'>
-      <h1 className='about-title'>About</h1>
-      <h5 className='about-text'>
-        Welcome to DeliCious, the place for finding the best food in Washington
-        D.C.
-      </h5>
-      <div className='wash'>
-        <img
-          className='img-abt'
-          src={Washington}
-          alt='washington monument'
-        ></img>
+    <div className="about-page-container">
+      <h1 className="about-header-title">About</h1>
+      <h1 className="about-header-subtitle">
+        Helping you find the best restaurants in DC
+      </h1>
+      <div className="about-container">
+        <h4 className="about-words-top">
+          <span className="about-big-d">D</span>e
+          <span className="about-big-l">L</span>icious is here to help you find
+          the best place to eat when you're in D.C. Are you from D.C. and trying
+          to find a new place to have a nice meal? Look no further! Using
+          DeLicious you can search through 100 different restaurants, anything
+          from Japanese Sushi restaurants to Greek food!
+        </h4>
+        <div className="about-image-container">
+          <img className="wash-image-one" src={Washington} />
+        </div>
+      </div>
+      <div className="about-container">
+        <div className="about-image-container-middle">
+          <img className="wash-image-one" src={Washington} />
+        </div>
+        <h4 className="about-words-top">
+          <span className="about-big-d">D</span>e
+          <span className="about-big-l">L</span>icious is here to help you find
+          the best place to eat when you're in D.C. Are you from D.C. and trying
+          to find a new place to have a nice meal? Look no further! Using
+          DeLicious you can search through 100 different restaurants, anything
+          from Japanese Sushi restaurants to Greek food!
+        </h4>
+      </div>
+      <div className="about-container">
+        <h4 className="about-words-top">
+          <span className="about-big-d">D</span>e
+          <span className="about-big-l">L</span>icious is here to help you find
+          the best place to eat when you're in D.C. Are you from D.C. and trying
+          to find a new place to have a nice meal? Look no further! Using
+          DeLicious you can search through 100 different restaurants, anything
+          from Japanese Sushi restaurants to Greek food!
+        </h4>
+        <div className="about-image-container">
+          <img className="wash-image-one" src={Washington} />
+        </div>
       </div>
     </div>
   );
